f450c62fc3f1e1a2ccece24975b669a02571a9df
ericmartineau <smartytime@gmail.com>
2021-12-01 14:02:51 -0700

    Prepare for publish

--- lib/src/helper/util.dart
+++ lib/src/helper/util.dart
@@ -4,7 +4,7 @@ extension formattedString on List<String> {
   String? get toFormattedString {
     String? formattedText;
     this.forEach((val) {
-      formattedText = formattedText == null ? val + ';' : formattedText! + val + ";";
+      formattedText = formattedText == null ? val + ',' : formattedText! + val + ",";
     });
     return formattedText;
   }
